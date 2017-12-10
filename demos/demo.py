65ecd0515fe22361724eb284d1e67ba2b71591fa
Manoel Stilpen <mstilpen15@gmail.com>
2017-12-09 22:49:23 -0200

    ajuste python demo

--- demos/demo.py
+++ demos/demo.py
@@ -36,34 +36,37 @@
 #********************************************************************/
 
 import reeds_shepp
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
-
-step_size = 0.5
+step_size = 2
 rho = 5.8 # turning radius
 
 qs = [
-    [( 0.0, 0.0,     0.0), ( 0.0, 0.0,   np.pi)],
-    [( 0.0, 0.0, np.pi/4), ( 3.0, 4.0,     0.0)],
-    [( 4.0, 4.0, np.pi/4), ( 0.0, 4.0,     0.0)],
-    [( 4.0, 0.0,     0.0), (-3.0, 4.0,   np.pi)],
-    [(-4.0, 0.0,     0.0), ( 3.0, 4.0, np.pi/3)],
-    [( 4.0, 4.0,     0.0), ( 3.0, 4.0, np.pi/2)]
+    [( 20, 70, math.radians(90)), (30, 50, math.radians(270))]
 ]
 
+#qs = [
+#    [( 0.0, 0.0,     0.0), ( 0.0, 0.0,   np.pi)],
+#    [( 0.0, 0.0, np.pi/4), ( 3.0, 4.0,     0.0)],
+#    [( 4.0, 4.0, np.pi/4), ( 0.0, 4.0,     0.0)],
+#    [( 4.0, 0.0,     0.0), (-3.0, 4.0,   np.pi)],
+#    [(-4.0, 0.0,     0.0), ( 3.0, 4.0, np.pi/3)],
+#    [( 4.0, 4.0,     0.0), ( 3.0, 4.0, np.pi/2)]
+#]
+
 def get_point(center, radius, orin):
-	x = center[0] + radius * np.cos(orin)
-	y = center[1] + radius * np.sin(orin)
-	return (x,y)
+    x = center[0] + radius * np.cos(orin)
+    y = center[1] + radius * np.sin(orin)
+    return (x, y)
 
 def plot_car(q):
     a = get_point(q[:-1], step_size, q[2])
     b = get_point(q[:-1], step_size/2, q[2]+150./180.*np.pi)
     c = get_point(q[:-1], step_size/2, q[2]-150./180.*np.pi)
-    tri = np.array([a,b,c,a])
-    plt.plot(tri[:,0], tri[:,1], 'g-')
+    tri = np.array([a, b, c, a])
+    plt.plot(tri[:, 0], tri[:, 1], 'g-')
 
 def plot_path(q0, q1):
     qs = reeds_shepp.path_sample(q0, q1, rho, step_size)
@@ -82,9 +85,11 @@ def plot_table(cols):
         plt.subplot(rows, cols, i+1)
         plot_path(q0, q1)
         dist = reeds_shepp.path_length(q0, q1, rho)
+        print dist
+        print str(q0) + " " + str(q1)
         plt.title('length: {:.2f}'.format(dist))
     plt.savefig('fig/demo.png')
     plt.show()
 
 if __name__ == "__main__":
-    plot_table(3)
+    plot_table(1)
